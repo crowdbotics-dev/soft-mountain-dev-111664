02f80f36e48d593bd375d0b169b64b532386e436
Crowdbotics <engineering@crowdbotics.com>
2023-08-26 00:53:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-mountain-dev-111664",
+  url: "https://soft-mountain-dev-111664.botics.co",
+  api: "https://soft-mountain-dev-111664.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
